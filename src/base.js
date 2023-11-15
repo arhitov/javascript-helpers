68a49711ddf2a9dfd77521d35ecf9f63e741fd82
clgsru <clgsru@gmail.com>
2023-11-15 23:34:47 +0300

    Add function parent, parentOrFail, generateIdx, sleep, dump, dd. Extend fetchJsonHandler, listenerEvent

--- src/base.js
+++ src/base.js
@@ -1,6 +1,12 @@
 'use strict';
 
 /**
+ * Basic assistant functions
+ * @see https://github.com/arhitov/javascript-helpers
+ * @author Alexander Arhitov clgsru@gmail.com
+ */
+
+/**
  * Создание события
  * @param {string} name
  * @param {Object|Array} data
@@ -14,13 +20,14 @@ const callEvent = (name, data = null, object = document) => {
  * Подписка на событие
  * @param {string} type
  * @param {function} callback
- * @param {Element|Document|string} object объект наблюдения
+ * @param {Element|Document|Window|string} object объект наблюдения
+ * @param {Element|Document} parent элемент в котором производится поиск
  */
-const listenerEvent = (type, callback, object = document) => {
+const listenerEvent = (type, callback, object = document, parent = document) => {
     if ('string' === typeof object) {
         selectAllForEach(object, element => {
             listenerEvent(type, callback, element);
-        });
+        }, parent);
     } else {
         object.addEventListener(type, event => {
             const detail = event?.detail;
@@ -34,7 +41,6 @@ const listenerEvent = (type, callback, object = document) => {
     }
 };
 
-
 /**
  * Подписка на событие загрузки страницы
  * @param {function} callback
@@ -47,7 +53,8 @@ const readyEvent = callback => {
 /**
  * Получить элемент по селектору
  * @param {string} selector
- * @param {Element|Document} parent объект наблюдения
+ * @param {Element|Document} parent элемент в котором производится поиск
+ * @return Element|null
  */
 const select = (selector, parent = document) => {
     return parent.querySelector(selector);
@@ -56,10 +63,37 @@ const select = (selector, parent = document) => {
 /**
  * Получить элемент по селектору
  * @param {string} selector
- * @param {Element|Document} parent объект наблюдения
+ * @param {Element|Document} parent элемент в котором производится поиск
+ * @return Element
+ * @throws Error
  */
 const selectOrFail = (selector, parent = document) => {
-    const element = parent.querySelector(selector);
+    const element = select(selector, parent);
+    if (null === element) {
+        throw Error('Error: DOM Element not found!')
+    }
+    return element;
+};
+
+/**
+ * Получить родительский элемент по селектору
+ * @param {Element} node элемент от которого производится поиск
+ * @param {string} selector
+ * @return Element|null
+ */
+const parent = (node, selector) => {
+    return node.closest(selector);
+};
+
+/**
+ * Получить родительский элемент по селектору
+ * @param {Element} node элемент от которого производится поиск
+ * @param {string} selector
+ * @return Element
+ * @throws Error
+ */
+const parentOrFail = (node, selector) => {
+    const element = parent(node, selector);
     if (null === element) {
         throw Error('Error: DOM Element not found!')
     }
@@ -86,6 +120,14 @@ const selectAllForEach = (selector, callback, parent = document) => {
     });
 };
 
+class ErrorData extends Error {
+    data;
+    constructor(message, data) {
+        super(message);
+        this.data = data;
+    }
+}
+
 /**
  * Basic JSON promise fetch handler
  * @param {Promise} promise
@@ -99,8 +141,15 @@ const fetchJsonHandler = async (promise) => {
                     answer: await response.json()
                 };
             } catch (e) {
-                console.error(e);
-                throw new Error(`${response.status} ${response.statusText} ${response.url}`);
+                if ([200, 201, 202, 204].indexOf(response.status)) {
+                    return {
+                        response: response,
+                        answer: {}
+                    };
+                } else {
+                    console.error(e);
+                    throw new ErrorData(`${response.status} ${response.statusText} ${response.url}`, response);
+                }
             }
         })
         .then(data => {
@@ -111,13 +160,42 @@ const fetchJsonHandler = async (promise) => {
                     answer: data.answer
                 };
             } else if (data.answer.message) {
-                throw new Error(`${data.answer.message}`);
+                throw new ErrorData(`${data.answer.message}`, data);
             } else {
-                throw new Error(`${data.response.status} ${data.response.statusText} ${data.response.url}`);
+                throw new ErrorData(`${data.response.status} ${data.response.statusText} ${data.response.url}`, data);
             }
         })
-        .catch((error) => {
-            console.error(error);
-            throw new Error(error);
+        .catch(error => {
+            throw new ErrorData(error, error.data ?? null);
         });
 };
+
+/**
+ * Генерирует случайны 4-х значный идентификатор
+ * @return {number}
+ */
+const generateIdx = () => {
+    return Math.floor(Math.random() * (9999 - 1000) + 1000);
+};
+
+const sleep = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const dump = function () {
+    console.log('Dump:');
+    for (let i = 0; i < arguments.length; i++) {
+        console.log(`#${i}`, arguments[i]);
+    }
+    console.trace();
+}
+
+const dd = function () {
+    dump(...arguments);
+    throw new (class Die extends Error{
+        constructor(message) {
+            super(message);
+            this.name = 'Die';
+        }
+    })('Break!');
+}
